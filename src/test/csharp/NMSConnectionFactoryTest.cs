b55abb8ad77b39398c2d1d144cd848a2e105f563
Timothy A. Bish <tabish@apache.org>
2010-01-12 00:37:58 +0000

    port code to build on .NET CF 2.0 and above

--- src/test/csharp/NMSConnectionFactoryTest.cs
+++ src/test/csharp/NMSConnectionFactoryTest.cs
@@ -43,8 +43,9 @@ namespace Apache.NMS.Stomp.Test
         [Row("discovery://${activemqhost}:6155", ExpectedException = typeof(NMSConnectionException))]
         [Row("sms://${activemqhost}:61613", ExpectedException = typeof(NMSConnectionException))]
         [Row("stomp:multicast://${activemqhost}:6155", ExpectedException = typeof(NMSConnectionException))]
+#if !NETCF
         [Row("stomp:(tcp://${activemqhost}:61613)?connection.asyncSend=false", ExpectedException = typeof(NMSConnectionException))]
-
+#endif
         [Row("(tcp://${activemqhost}:61613,tcp://${activemqhost}:61613)", ExpectedException = typeof(UriFormatException))]
         [Row("tcp://${activemqhost}:61613,tcp://${activemqhost}:61613", ExpectedException = typeof(UriFormatException))]
         public void TestURI(string connectionURI)

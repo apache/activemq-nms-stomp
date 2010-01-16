4b981ae9a41b0c595ea86cfb1570b3cc328d7153
Jim Gomes <jgomes@apache.org>
2010-01-16 00:32:33 +0000

    Updated package scripts.

--- package.ps1
+++ package.ps1
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 $pkgname = "Apache.NMS.Stomp"
-$pkgver = "1.3-SNAPSHOT"
+$pkgver = "1.2.x"
 $configurations = "release", "debug"
 $frameworks = "mono-2.0", "net-2.0", "net-3.5", "netcf-2.0"
 
